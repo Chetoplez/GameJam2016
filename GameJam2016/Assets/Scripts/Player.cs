df69cdfbc930e971de71b9e3351da7a01de3866a
Francesco Russo <frusso.d1989@gmail.com>
2016-01-30 13:24:57 +0100

    Player follow path

--- GameJam2016/Assets/Scripts/Player.cs
+++ GameJam2016/Assets/Scripts/Player.cs
@@ -2,46 +2,65 @@
 using System.Collections;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
     public Rigidbody2D PlayerRigidbody;
+    public BoxCollider2D PlayerCollider;
     [Range(1, 10)]
     public float WalkSpeed = 1f;
     [Range(1, 50)]
     public float JumpSpeed = 1f;
-    [Range(0f,1.0f)]
+    [Range(0f, 1.0f)]
     public float FallSpeedFactor = 0.5F;
-    [Range(0.25f,1f)]
+    [Range(0.25f, 1f)]
     public float JumpDelay = 0.5f;
 
-    private bool mCanJump=true;
+
+    /// <summary>
+    /// This is our path
+    /// </summary>
+    public GameObject[] PathPoints;
+    public float Threshold = 0.2f;
+
+
+    private bool mCanJump = true;
     private float mJumpTimer = 0f;
-    
+
     private bool mIsGrounded = true;
     public bool IsGrounded { get { return mIsGrounded; } }
 
-    private Vector3 mJumpVector=Vector3.zero;
+    private Vector3 mJumpVector = Vector3.zero;
     private Vector3 mStartingPosition;
     private bool mIsFacingRight = true;
-    
+
     private bool mIsDead = false;
     public bool IsDead { get { return mIsDead; } }
     private Vector3 mContactNormal = Vector3.zero;
     private Vector3 mInitialScale = Vector3.zero;
 
 
+    private bool mAnimationDone = false;
+    private Vector3 mNextPoint = Vector3.zero;
+    private int mNextIndex = 0;
+    private float mInitialSpeed = 1f;
 
     #region StateMachine
 
-    void Start () {
+    void Start()
+    {
         mJumpVector.y = JumpSpeed;
         mJumpTimer = JumpDelay;
         mStartingPosition = this.transform.position;
         mInitialScale = this.transform.localScale;
-	}
-	
-	
-	void Update () {
+        Debug.Log("Player: start animation");
+        this.PlayerRigidbody.isKinematic = true;
+        this.PlayerCollider.enabled = false;
+    }
+
+
+    void FixedUpdate()
+    {
         if (!mCanJump)
         {
             if (mJumpTimer >= 0)
@@ -50,8 +69,15 @@ public class Player : MonoBehaviour {
                 mCanJump = true;
         }
 
-        
-	}
+        if (!mAnimationDone)
+        {
+            float step = mInitialSpeed * Time.deltaTime;
+            this.transform.Translate(Time.deltaTime * (PathPoints[mNextIndex].transform.position - transform.position));
+            if (Vector3.Distance(transform.position, PathPoints[mNextIndex].transform.position) < Threshold)
+                NextPoint();
+        }
+
+    }
 
 
 
@@ -67,10 +93,10 @@ public class Player : MonoBehaviour {
                 mContactNormal.x = collision.contacts[0].normal.x;
                 mContactNormal.y = collision.contacts[0].normal.y;
 
-                float angle = Vector3.Dot((mIsFacingRight) ? this.transform.right : - this.transform.right, mContactNormal);
+                float angle = Vector3.Dot((mIsFacingRight) ? this.transform.right : -this.transform.right, mContactNormal);
                 angle = ((mIsFacingRight) ? -angle : angle);
-               
-                Quaternion rotation = Quaternion.AngleAxis(angle*25, Vector3.forward);
+
+                Quaternion rotation = Quaternion.AngleAxis(angle * 25, Vector3.forward);
                 this.transform.rotation = rotation;
 
             }
@@ -95,14 +121,16 @@ public class Player : MonoBehaviour {
     /// <summary>
     /// Add force to our player
     /// </summary>
-    private void AddForce(Vector3 force) {
+    private void AddForce(Vector3 force)
+    {
         this.PlayerRigidbody.AddForce(force * FallSpeedFactor, ForceMode2D.Impulse);
     }
 
     /// <summary>
     /// The player jumps!
     /// </summary>
-    public void Jump() {
+    public void Jump()
+    {
         if (mCanJump)
         {
             AddForce(mJumpVector);
@@ -114,9 +142,10 @@ public class Player : MonoBehaviour {
     /// <summary>
     /// Move the player!
     /// </summary>
-    public void Move(Vector3 move,bool left=true) {
+    public void Move(Vector3 move, bool left = true)
+    {
         //If dead, do not anything!
-        if (mIsDead) 
+        if (mIsDead || !mAnimationDone)
             return;
 
         mIsFacingRight = this.transform.localScale.x > 0;
@@ -129,7 +158,8 @@ public class Player : MonoBehaviour {
     /// <summary>
     /// Flip the character due to our direction
     /// </summary>
-    private void Flip() {
+    private void Flip()
+    {
         transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
 
@@ -137,31 +167,52 @@ public class Player : MonoBehaviour {
     /// <summary>
     /// Reset all our status
     /// </summary>
-    public void Reset() {
+    public void Reset()
+    {
         this.transform.position = mStartingPosition;
         this.mIsDead = false;
-        this.transform.localScale= mInitialScale;
+        this.transform.localScale = mInitialScale;
+        this.mNextIndex = 0;
+        this.mAnimationDone = false;
+        this.PlayerRigidbody.isKinematic = true;
+        this.PlayerCollider.enabled = false;
     }
 
     /// <summary>
     /// Die player, die!
     /// </summary>
-    public void Die() {
+    public void Die()
+    {
         this.mIsDead = true;
-        GameController gc = FindObjectOfType<GameController>();
-        if (gc != null) {
-            gc.PlayerDie();
-        }
     }
 
 
-    public void ChangeScale(float newscale) {
+    public void ChangeScale(float newscale)
+    {
         Vector3 newVectorScale = Vector3.zero;
         newVectorScale.z = this.mInitialScale.z;
         newVectorScale.x = this.mInitialScale.x * newscale;
         newVectorScale.y = this.mInitialScale.y * newscale;
         this.transform.localScale = newVectorScale;
-    
+
+    }
+
+
+
+    public void NextPoint()
+    {
+        mNextIndex++;
+
+        if (mNextIndex == 3 || mNextIndex == 6)
+            Flip();
+
+        if (mNextIndex >= (PathPoints.Length - 1))
+        {
+            mAnimationDone = true;
+            this.PlayerRigidbody.isKinematic = false;
+            this.PlayerCollider.enabled = true;
+            Debug.Log("Animation done! Now you can walk!");
+        }
     }
 
 
