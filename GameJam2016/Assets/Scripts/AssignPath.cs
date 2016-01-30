a6b557f3568c972e3e06c4ba1ad65a2cdc32c17e
Francesco Russo <frusso.d1989@gmail.com>
2016-01-30 15:07:33 +0100

    Added transition bool

--- GameJam2016/Assets/Scripts/AssignPath.cs
+++ GameJam2016/Assets/Scripts/AssignPath.cs
@@ -4,15 +4,26 @@ using System.Collections;
 public class AssignPath : MonoBehaviour {
 
     public GameObject[] PathPoints;
+    public bool TransitionDuringPath = false;
+    public SimpleCameraFollow MainCamera;
+
+
     private bool mPlayerPassed = false;
     private Player MainPlayer = null;
+
+    void Start() {
+        if (MainCamera == null)
+            Debug.LogError("AssignPath: MainCamera is not assigned!");
+    }
 
     void OnTriggerEnter2D(Collider2D other) {
         if (Keys.IsPlayer(other.gameObject) && !mPlayerPassed)
         {
             if(MainPlayer==null)
                 MainPlayer= other.gameObject.GetComponent<Player>();
-            
+            if (TransitionDuringPath)
+                MainCamera.ActivateTransition();
+
             MainPlayer.AssignPath(PathPoints);
             mPlayerPassed = true;
         }
