4fb6c4a5f6ae9f56cd7d88916a4ca42f6a06e892
Francesco Russo <frusso.d1989@gmail.com>
2016-01-31 05:05:18 +0100

    Bug fixes #4

---
+++ GameJam2016/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinTrigger : MonoBehaviour {
+
+    public GameController MainGameController;
+    public GameObject FinalImage;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (Keys.IsPlayer(other.gameObject) && MainGameController.isCorrect)
+        {
+            FinalImage.SetActive(true);
+        }
+    }
+
+}
